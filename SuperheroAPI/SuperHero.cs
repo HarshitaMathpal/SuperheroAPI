0bbfd01673fd4b2709aff746f037739804eb5fdf
Harshita_Mathpal <mathpalharshita45@gmail.com>
2023-10-12 15:49:23 +0530

    Crud Operations

--- SuperheroAPI/SuperHero.cs
+++ SuperheroAPI/SuperHero.cs
@@ -3,6 +3,8 @@
     public class SuperHero
     {
         public int Id { get; set; }
+
+
         public string Name { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
